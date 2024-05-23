326c4a0b685131e07a0f30faaffb947bfe8bf303
Felix Bohn <felix.bohn@xitaso.com>
2024-05-23 15:14:34 +0200

    feat: add axios for ucs api requests

--- licence-connect/src/licences/licences.module.ts
+++ licence-connect/src/licences/licences.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { LicencesController } from './licences.controller';
 import { LicenceService } from './licences.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule],
   controllers: [LicencesController],
   providers: [LicenceService],
 })
--- licence-connect/src/licences/licences.service.ts
+++ licence-connect/src/licences/licences.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { Licences } from './types';
+import { HttpService } from '@nestjs/axios';
 
 @Injectable()
 export class LicenceService {
+  constructor(private readonly httpService: HttpService) {}
+
   getLicences(): Licences {
     return { hasLicence: false, licences: [] };
   }
