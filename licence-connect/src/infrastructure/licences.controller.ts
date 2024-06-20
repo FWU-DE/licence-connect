aef1fb8c7318ef055de996400dddf1683fd8cd0e
Felix Bohn <felix.bohn@xitaso.com>
2024-06-20 10:06:57 +0200

    refactor: change api call method to post

--- licence-connect/src/infrastructure/licences.controller.ts
+++ licence-connect/src/infrastructure/licences.controller.ts
@@ -1,18 +1,18 @@
-import { Controller, Get, Req } from '@nestjs/common';
+import { Controller, Post, Req } from '@nestjs/common';
 import { RequestFromVidisCore } from '../domain/request-from-vidis-core';
 import { LicensesModel } from './LicencesModel';
 import { UCSLicenceFetcherService } from '../infrastructure/ucs/ucs-license-fetcher-service/ucs-license-fetcher-service.service';
 import { LicencesFromUcsStudendUseCase } from '../usecases/licences-from-ucs-student-use-case';
 import { UCSStudentFromUCSStudentId } from '../usecases/ucs-student-from-ucs-student-id';
 
-@Controller('licence')
+@Controller('licences')
 export class LicencesController {
   constructor(
     private readonly ucsLicenceFetcherService: UCSLicenceFetcherService,
   ) {}
 
   // TODO: Add Authentication
-  @Get()
+  @Post()
   public getLicences(
     @Req() request: { body: RequestFromVidisCore },
   ): LicensesModel {
