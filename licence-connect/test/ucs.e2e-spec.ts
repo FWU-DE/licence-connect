bceaf50f19671119e1a1fc877c213e1a1f536721
Felix Bohn <felix.bohn@xitaso.com>
2024-08-27 13:00:05 +0200

    feat: add e2e test for ucs

--- licence-connect/test/ucs.e2e-spec.ts
+++ licence-connect/test/ucs.e2e-spec.ts
@@ -4,32 +4,47 @@ import { INestApplication } from '@nestjs/common';
 import { AppModule } from '../src/app.module';
 import { RequestFromVidisCore } from '@vidis/domain/request-from-vidis-core';
 import { TEST_ENV_VARIABLES } from './test-env';
-import { HttpService } from '@nestjs/axios';
+import { UCSLicenseFetcherService } from '@ucs/infrastructure/ucs-license-fetcher-service/ucs-license-fetcher-service.service';
+import { createUcsStudentData } from './ucs-student-data';
+import { of } from 'rxjs';
+
+const ucsLicenceKey = TEST_ENV_VARIABLES.VIDIS_API_KEY;
 
 describe('UCS (e2e)', () => {
   let app: INestApplication;
-
-  const vidisApiKey = TEST_ENV_VARIABLES.VIDIS_API_KEY;
 
   process.env = { ...process.env, ...TEST_ENV_VARIABLES };
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-    }).compile();
+    })
+      .overrideProvider(UCSLicenseFetcherService)
+      .useValue({
+        getUCSStudentFromId: jest.fn((studentId: string) =>
+          of(createUcsStudentData(studentId)),
+        ),
+      })
+      .compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  const requestWithNonExistingUser: RequestFromVidisCore = {
+  const requestFromVidis: RequestFromVidisCore = {
     userId: 'student.42',
     clientId: 'sodix-editor-o',
     schulkennung: 'DE-RP-SN-51201',
     bundesland: 'DE-MV',
   };
 
-  describe('API KEY v1/licence (POST)', () => {
-    it('Test', () => {});
+  describe('API KEY v1/ucs/request', () => {
+    it('Test', () => {
+      return request(app.getHttpServer())
+        .post(`/ucs/request?X-API-KEY=${ucsLicenceKey}`)
+        .send(requestFromVidis)
+        .expect(200)
+        .expect(createUcsStudentData('student.42'));
+    });
   });
 });
