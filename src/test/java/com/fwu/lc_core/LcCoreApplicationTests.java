b0c988a8d0d2f8ea3516b4dbc9892e6fe50a9fc0
Florian Göttl <florian.goettl@xitaso.com>
2025-01-29 17:24:50 +0100

    refactor: switch over to Java

---
+++ src/test/java/com/fwu/lc_core/LcCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fwu.lc_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LcCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
