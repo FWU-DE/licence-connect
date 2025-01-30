2adb5e771d1585586f1cf34400e9682dbaf79efb
Frederik Schlüter <frederik.schlueter@xitaso.com>
2025-01-30 13:41:31 +0100

    refactor: ApiTokenAuthenticationFilter

--- src/main/java/com/fwu/lc_core/config/security/ApiTokenAuthenticationFilter.java
+++ src/main/java/com/fwu/lc_core/config/security/ApiTokenAuthenticationFilter.java
@@ -4,12 +4,6 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -18,32 +12,23 @@ public class ApiTokenAuthenticationFilter extends OncePerRequestFilter {
 
     private final String theOnlyApiKeyAvailableOnTheSystem;
 
-    public ApiTokenAuthenticationFilter(
-            String theOnlyApiKeyAvailableOnTheSystem
-    ) {
+    public ApiTokenAuthenticationFilter(String theOnlyApiKeyAvailableOnTheSystem) {
         this.theOnlyApiKeyAvailableOnTheSystem = theOnlyApiKeyAvailableOnTheSystem;
     }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        System.out.println("I do internal Filtering!");
         String apiKey = extractApiKey(request);
-        if (apiKey != null && apiKey.equals(theOnlyApiKeyAvailableOnTheSystem)) {
-            System.out.println("yay api key is correct!");
+        if (apiKey != null && apiKey.equals(theOnlyApiKeyAvailableOnTheSystem))
             filterChain.doFilter(request, response);
-        }
-        else {
-            System.out.println("nay api key is incorrect!");
+        else
             response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-        }
     }
 
-    private String extractApiKey(HttpServletRequest request) {
-        String apiKey = request.getHeader("x-api-key");
-        if (apiKey == null) {
-            apiKey = request.getHeader("X-API-KEY");
-        }
-        return apiKey;
+    private static String extractApiKey(HttpServletRequest request) {
+        String headerLowerCase = request.getHeader("x-api-key");
+        String headerUpperCase = request.getHeader("X-API-KEY");
+        return headerLowerCase != null ? headerLowerCase : headerUpperCase;
     }
 }
