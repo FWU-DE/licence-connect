fa2ae8edb24bc00a94fda221f1eaca0c1f1d0ff8
Frederik Schlüter <frederik.schlueter@xitaso.com>
2025-01-30 13:21:16 +0100

    feat(SecurityConfig): Add api token filter

--- src/main/java/com/fwu/lc_core/bilov1/BiloV1Controller.java
+++ src/main/java/com/fwu/lc_core/bilov1/BiloV1Controller.java
@@ -1,7 +1,10 @@
 package com.fwu.lc_core.bilov1;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class BiloV1Controller {
     @PostMapping("/v1/ucs/request")
     private String request() {
