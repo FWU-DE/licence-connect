fac9ce9523790d2286c82ab6dff04cfd340588ec
Florian Göttl <florian.goettl@xitaso.com>
2025-02-01 12:41:46 +0100

    feat: switch to using flux

--- src/main/java/com/fwu/lc_core/licences/LicencesController.java
+++ src/main/java/com/fwu/lc_core/licences/LicencesController.java
@@ -2,17 +2,19 @@ package com.fwu.lc_core.licences;
 
 import com.fwu.lc_core.licences.models.Licence;
 import com.fwu.lc_core.licences.models.LicencesRequestParams;
+import com.fwu.lc_core.licences.models.UnparsedLicences;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
-import java.util.List;
 
 @RestController
 public class LicencesController {
     @PostMapping("/v1/licenses/request")
     private String request(LicencesRequestParams params) {
-        List<Licence> licenses = LicencesParser.parse(LicencesCollector.getUnparsedLicences(params));
-        // TODO: Convert to DTO
+
+        Flux<Licence> licenses = LicencesCollector.getUnparsedLicences(params).flatMap(LicencesParser::parse);
+        // TODO: Convert to DTO/needed Data format.
         // TODO: Return licenses
         return "";
     }
--- src/main/java/com/fwu/lc_core/licences/LicencesParser.java
+++ src/main/java/com/fwu/lc_core/licences/LicencesParser.java
@@ -1,12 +1,11 @@
 package com.fwu.lc_core.licences;
 
 import com.fwu.lc_core.licences.models.Licence;
-import com.fwu.lc_core.licences.models.UnparsedLicence;
-
-import java.util.List;
+import com.fwu.lc_core.licences.models.UnparsedLicences;
+import reactor.core.publisher.Flux;
 
 public class LicencesParser {
-    public static List<Licence> parse(List<UnparsedLicence> unparsedLicences) {
-        return List.of();
+    public static Flux<Licence> parse(UnparsedLicences unparsedLicences) {
+        return Flux.empty();
     }
 }
