e75704c70039d928aef2c052398c7c86c491caf5
Jan Nash <jan.nash@xitaso.com>
2025-02-05 10:13:45 +0100

    refactor: rename ApiTokenAuthenticationFilter

---
+++ src/main/java/com/fwu/lc_core/config/security/ApiKeyAuthenticationFilter.java
@@ -0,0 +1,60 @@
+package com.fwu.lc_core.config.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collection;
+
+public class ApiKeyAuthenticationFilter extends OncePerRequestFilter {
+
+    private final String apiKey;
+
+    public ApiKeyAuthenticationFilter(String apiKey) {
+        this.apiKey = apiKey;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String apiKey = extractApiKey(request);
+        if (apiKey != null && apiKey.equals(this.apiKey)) {
+            SecurityContextHolder.getContext().setAuthentication(new ApiKeyAuthenticationToken(apiKey, AuthorityUtils.NO_AUTHORITIES));
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private static String extractApiKey(HttpServletRequest request) {
+        String headerLowerCase = request.getHeader("x-api-key");
+        String headerUpperCase = request.getHeader("X-API-KEY");
+        return headerLowerCase != null ? headerLowerCase : headerUpperCase;
+    }
+}
+
+class ApiKeyAuthenticationToken extends AbstractAuthenticationToken {
+    private final String apiKey;
+
+    public ApiKeyAuthenticationToken(String apiKey, Collection<? extends GrantedAuthority> authorities) {
+        super(authorities);
+        this.apiKey = apiKey;
+        setAuthenticated(true);
+    }
+
+    @Override
+    public Object getCredentials() {
+        return null;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return apiKey;
+    }
+}
