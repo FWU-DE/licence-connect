a74a3aa4059cb3c8877e825f75db937a368c5c72
Jan Nash <jan.nash@xitaso.com>
2025-02-11 10:55:25 +0100

    cd: specify server url for swagger UI

--- src/main/java/com/fwu/lc_core/config/swagger/SwaggerConfig.java
+++ src/main/java/com/fwu/lc_core/config/swagger/SwaggerConfig.java
@@ -4,8 +4,11 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -20,6 +23,8 @@ public class SwaggerConfig {
                                 .name("X-API-KEY")
                         )
                 )
-                .addSecurityItem(new SecurityRequirement().addList("apiKey"));
+                .addSecurityItem(new SecurityRequirement().addList("apiKey"))
+                // TODO: Replace with env var
+                .servers(List.of(new Server().url("https://api.licenceconnect.schule")));
     }
 }
