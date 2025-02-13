d5cafac7621044ad0dcf763a47691d7f4fd1d1ec
Frederik Schlüter <frederik.schlueter@xitaso.com>
2025-02-13 16:57:16 +0100

    fix(mock-servers): Apply review comments to bilo v1 mock server

--- src/mock-licence-servers/bilo-v1/mock_bilo_v1_server.py
+++ src/mock-licence-servers/bilo-v1/mock_bilo_v1_server.py
@@ -2,13 +2,15 @@ from flask import Flask, jsonify, request, Response
 
 app = Flask(__name__)
 
+mock_access_token_value = "mock_access_token"
+
 @app.route('/ucsschool/apis/auth/token', methods=['GET', "POST"])
 def token():
     username = request.form.get('username')
     password = request.form.get('password')
     if username == config["bilo.v1.auth.admin.username"] and password == config["bilo.v1.auth.admin.password"]:
         return jsonify({
-            "access_token": "mock_access_token",
+            "access_token": mock_access_token_value,
             "token_type": "Bearer",
             "expires_in": 3600
         })
@@ -17,7 +19,7 @@ def token():
 
 @app.route('/ucsschool/apis/bildungslogin/v1/user/<user_id>', methods=['GET'])
 def licences(user_id):
-    if request.headers.get('Authorization') == 'Bearer mock_access_token' and user_id == 'student.2':
+    if request.headers.get('Authorization') == 'Bearer ' + mock_access_token_value and user_id == 'student.2':
         return jsonify({
             "id": user_id,
             "first_name": "student",
@@ -44,21 +46,20 @@ def licences(user_id):
     else:
         return Response("Unauthorized", status=401)
 
-def parse_file(file_path):
+def parse_local_section_profile_config_file(file_path):
     config = {}
     with open(file_path, 'r') as file:
+        has_reached_local_section = False
         for line in file:
-            if line.strip() and not line.startswith('#'):
+            if line.strip() == 'spring.config.activate.on-profile=local':
+                has_reached_local_section = True
+            if line.strip() and not line.startswith('#') and has_reached_local_section:
                 key, value = line.split('=', 1)
                 config[key.strip()] = value.strip()
-                if (key == "bilo.v1.auth.admin.password"):
-                    print("password found")
-                    print(key, value)
-                    print(config[key])
     return config
 
 if __name__ == '__main__':
     print("starting v1 mock")
     global config
-    config = parse_file('./application.properties')
+    config = parse_local_section_profile_config_file('./application.properties')
     app.run(debug=True, port=1237, host="0.0.0.0")
