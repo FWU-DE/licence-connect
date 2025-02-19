7f6efa02cf49810d7603b98c3af539029003aa29
franz.anders <franz.anders@xitaso.com>
2025-02-19 16:41:26 +0100

    feat: Autowire AcceptingArixClient

--- src/main/java/com/fwu/lc_core/licences/collection/arix/ArixController.java
+++ src/main/java/com/fwu/lc_core/licences/collection/arix/ArixController.java
@@ -2,6 +2,7 @@ package com.fwu.lc_core.licences.collection.arix;
 
 import com.fwu.lc_core.licences.models.UnparsedLicences;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -12,19 +13,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ArixController {
 
-    @Value("${mocks.arix.accepting.url}")
-    private String baseUrlAccepting;
+    @Autowired
+    ArixClientAccepting arixClientAccepting;
 
     @Validated
     @GetMapping("/arix/request")
     private ResponseEntity<String> request(@Valid @RequestBody ArixRequestDto request) {
-        ArixClient arixClientAccepting = new ArixClient(baseUrlAccepting);
 
         UnparsedLicences unparsedLicences = arixClientAccepting.getLicences(
                 request.bundesland,
                 request.standortnummer,
                 request.schulnummer,
                 request.userId).block();
+
         return ResponseEntity.ok(unparsedLicences.rawResult);
     }
 
