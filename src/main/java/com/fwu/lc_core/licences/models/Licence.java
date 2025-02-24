3c27dff3b84e184fbb4e8f6bc3679b651d57333d
Florian Göttl <florian.goettl@xitaso.com>
2025-02-24 15:10:11 +0100

    feat: connect licences/request endpoint to arix

--- src/main/java/com/fwu/lc_core/licences/models/Licence.java
+++ src/main/java/com/fwu/lc_core/licences/models/Licence.java
@@ -1,7 +1,9 @@
 package com.fwu.lc_core.licences.models;
 
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 @AllArgsConstructor
 public class Licence {
     public String licenceCode;
