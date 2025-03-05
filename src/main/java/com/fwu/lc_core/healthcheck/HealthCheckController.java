ef64b5e45714e42a4b648be2ae89fc399495b35a
Frederik Schlüter <frederik.schlueter@xitaso.com>
2025-03-05 13:22:25 +0100

    WIP feat: Add grafana, broken

--- src/main/java/com/fwu/lc_core/healthcheck/HealthCheckController.java
+++ src/main/java/com/fwu/lc_core/healthcheck/HealthCheckController.java
@@ -1,13 +1,18 @@
 package com.fwu.lc_core.healthcheck;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.ResponseEntity;
 
 @RestController
 public class HealthCheckController {
     @GetMapping("/v1/healthcheck")
     public ResponseEntity<String> healthcheck() {
+        Logger logger = LoggerFactory.getLogger(HealthCheckController.class);
+        logger.info("Health check request received");
+        logger.error("Health check request received");
         return ResponseEntity.ok("Application is up");
     }
 }
