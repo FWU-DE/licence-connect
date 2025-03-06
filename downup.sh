0114b6163d53f8ed14cd2ba3aefd20832afa9f7f
Frederik Schlüter <frederik.schlueter@xitaso.com>
2025-03-06 17:33:56 +0100

    refactor: Remove unnecessary debug artifacts

--- downup.sh
+++ downup.sh
@@ -1,7 +1,7 @@
 #/bin/bash -euo pipefail
 
 LC_IMAGE=lc_core:0.0.1-SNAPSHOT docker compose -f ./docker/docker-compose.yaml down
-docker build -t "mana" ./docker
 ./mvnw clean spring-boot:build-image -DskipTests
 LC_IMAGE=lc_core:0.0.1-SNAPSHOT docker compose -f ./docker/docker-compose.yaml up --build -d --wait
-docker exec -it licence-connect /bin/bash -c 'ls -halt /workspace/'
+echo -e $(curl -X GET http://localhost:8080/v1/healthcheck)
+
