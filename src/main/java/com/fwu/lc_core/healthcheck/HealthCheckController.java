58ffdce3eddb63cb7a555d1413f8990c3c171a74
Frederik Schlüter <frederik.schlueter@xitaso.com>
2025-03-07 15:04:14 +0100

    refactoring(HealthCheckController): Remove error log

--- src/main/java/com/fwu/lc_core/healthcheck/HealthCheckController.java
+++ src/main/java/com/fwu/lc_core/healthcheck/HealthCheckController.java
@@ -8,11 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthCheckController {
+    private final Logger logger = LoggerFactory.getLogger(HealthCheckController.class);
+
     @GetMapping("/v1/healthcheck")
     public ResponseEntity<String> healthcheck() {
-        Logger logger = LoggerFactory.getLogger(HealthCheckController.class);
         logger.info("Health check request received");
-        logger.error("Health check request received");
         return ResponseEntity.ok("Application is up");
     }
 }
