4b052ed3ad404136a3efae6a098c2809c77756fb
Frederik Schlueter <frederik.schlueter@xitaso.com>
2025-03-11 13:15:02 +0000

    Feat/flc 31 implement logging with grafana integration

--- src/main/java/com/fwu/lc_core/healthcheck/HealthCheckController.java
+++ src/main/java/com/fwu/lc_core/healthcheck/HealthCheckController.java
@@ -1,13 +1,18 @@
 package com.fwu.lc_core.healthcheck;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.ResponseEntity;
 
 @RestController
 public class HealthCheckController {
+    private final Logger logger = LoggerFactory.getLogger(HealthCheckController.class);
+
     @GetMapping("/v1/healthcheck")
     public ResponseEntity<String> healthcheck() {
+        logger.info("Health check request received");
         return ResponseEntity.ok("Application is up");
     }
 }
