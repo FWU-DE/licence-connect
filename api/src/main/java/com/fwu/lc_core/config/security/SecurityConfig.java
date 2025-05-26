17b6fb22a95f94a1e286741863f68b691093f6cd
Jan Nash <jan.nash@xitaso.com>
2025-05-26 14:09:45 +0000

    FLC-108 admin endpoints respond incorrectly with 200

--- api/src/main/java/com/fwu/lc_core/config/security/SecurityConfig.java
+++ api/src/main/java/com/fwu/lc_core/config/security/SecurityConfig.java
@@ -3,11 +3,13 @@ package com.fwu.lc_core.config.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -18,24 +20,23 @@ public class SecurityConfig {
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-
         http.
                 csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .exceptionHandling(exceptionHandlingSpec -> {
-                    exceptionHandlingSpec
-                            .authenticationEntryPoint((exchange, ex) -> {
-                                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
-                                return exchange.getResponse().setComplete();
-                            })
-                            .accessDeniedHandler((exchange, ex) -> {
-                                exchange.getResponse().setStatusCode(HttpStatus.FORBIDDEN);
-                                return exchange.getResponse().setComplete();
-                            });
-                })
                 .authorizeExchange(exchange -> exchange
                         .pathMatchers("/swagger", "/swagger-ui/**", "/v3/api-docs/**", "/v1/healthcheck").permitAll()
                         .pathMatchers("/admin/**").hasRole("ADMIN")
                         .anyExchange().authenticated()
+                )
+                // This exception handling dance is required so when executing an unauthenticated request in the browser
+                // against an endpoint that needs authorization, *no* HTTP Basic auth popup is displayed by the browser.
+                .exceptionHandling(spec -> spec
+                        .authenticationEntryPoint((exchange, ex) -> {
+                                var response = exchange.getResponse();
+                                response.setStatusCode(HttpStatus.UNAUTHORIZED);
+                                response.getHeaders().set(HttpHeaders.WWW_AUTHENTICATE, "None");
+                                exchange.mutate().response(response);
+                                return Mono.empty();
+                        })
                 )
                 .addFilterAt(apiKeyAuthenticationWebFilter, SecurityWebFiltersOrder.AUTHENTICATION);
 
