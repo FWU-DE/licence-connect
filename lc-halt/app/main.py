be343d33a47288cc4ef3f74b904e26b0bdde07a6
Paul Reinhardt <paul.reinhardt@xitaso.com>
2025-05-26 16:38:05 +0200

    feat/introduce-logging-with-configurable-log-levels

--- lc-halt/app/main.py
+++ lc-halt/app/main.py
@@ -1,6 +1,9 @@
 from typing import Annotated
 from pydantic import BaseModel
 from fastapi import FastAPI, Query
+import logging
+import sys
+import os
 
 app = FastAPI(
     title="LC Halt",
@@ -8,6 +11,20 @@ app = FastAPI(
     summary="LC Halt ist a licence holding system.",
     description="Using this API, media can be assigned to users, schools or federal states. Other services can then access this information and act accordingly.",
 )
+
+
+logger = logging.getLogger(__name__)
+
+logLevel = os.getenv("LC_HALT_LOG_LEVEL", "INFO")
+logger.setLevel(logLevel)
+
+stream_handler = logging.StreamHandler(sys.stdout)
+log_formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(name)s: %(message)s")
+stream_handler.setFormatter(log_formatter)
+logger.addHandler(stream_handler)
+
+logger.info("API is starting up")
+logger.info(f"Log level set to {logLevel}")
 
 
 class LicencedMedium(BaseModel):
@@ -46,6 +63,9 @@ async def read_assigned_licences(
 
     Provided with a userId and optionally a bundesland and/or schulnummer, LC-Halt will return all media the user is allowed to access.
     """
+    logger.info(
+        f"Received request with userId {userId}, bundesland {bundesland}, schulnummer {schulnummer}"
+    )
     return LicenceResponse(
         userId=userId, bundesland=bundesland, schulnummer=schulnummer, licencedMedia=[]
     )
