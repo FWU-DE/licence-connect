ed47c1789fccaa0cba69217fcd214ff8ce309d55
Paul Reinhardt <paul.reinhardt@xitaso.com>
2025-05-26 17:10:38 +0200

    Introduce first API Key mechanism

--- lc-halt/app/main.py
+++ lc-halt/app/main.py
@@ -1,16 +1,11 @@
 from typing import Annotated
 from pydantic import BaseModel
-from fastapi import FastAPI, Query
+from fastapi import Depends, FastAPI, Query, Request, Security, HTTPException, status
+from fastapi.security import APIKeyHeader
 import logging
+import signal
 import sys
 import os
-
-app = FastAPI(
-    title="LC Halt",
-    version="0.1.0",
-    summary="LC Halt ist a licence holding system.",
-    description="Using this API, media can be assigned to users, schools or federal states. Other services can then access this information and act accordingly.",
-)
 
 
 logger = logging.getLogger(__name__)
@@ -25,6 +20,38 @@ logger.addHandler(stream_handler)
 
 logger.info("API is starting up")
 logger.info(f"Log level set to {logLevel}")
+
+
+client_api_key = os.getenv("LC_HALT_CLIENT_API_KEY")
+if client_api_key is None:
+    logger.error("LC_HALT_CLIENT_API_KEY not configured! Shutting down...")
+    os.kill(os.getpid(), signal.SIGTERM)
+
+req_api_key = APIKeyHeader(name="x-api-key", auto_error=False)
+
+
+async def handle_api_key(req: Request, key: str = Security(req_api_key)):
+    if key is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="You need to provide an api key",
+        )
+
+    if client_api_key != key:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="You do not have permission to access this route",
+        )
+    yield key
+
+
+app = FastAPI(
+    title="LC Halt",
+    version="0.1.0",
+    summary="LC Halt ist a licence holding system.",
+    description="Using this API, media can be assigned to users, schools or federal states. Other services can then access this information and act accordingly.",
+    dependencies=[Depends(handle_api_key)],
+)
 
 
 class LicencedMedium(BaseModel):
