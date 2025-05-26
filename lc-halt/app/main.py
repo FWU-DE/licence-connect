68ae5e178fc3db52ee5b4b3a8a906f8ddc5f4a59
Paul Reinhardt <paul.reinhardt@xitaso.com>
2025-05-26 13:28:54 +0200

    Improve OpenAPi spec

--- lc-halt/app/main.py
+++ lc-halt/app/main.py
@@ -1,11 +1,17 @@
 from typing import Annotated
 from pydantic import BaseModel
 from fastapi import FastAPI, Query
+from fastapi.openapi.utils import get_openapi
 
 app = FastAPI()
 
 
 class LicencedMedium(BaseModel):
+    """
+    Represents one licenced medium.
+    Further information regarding the licencing of this medium may be added here.
+    """
+
     id: str
 
 
@@ -34,8 +40,25 @@ async def read_assigned_licences(
     """
     Licences managed by LC-Halt can be retrieved using this endpoint.
 
-    Provided with a userId and optionally a bundesland and/or schulnummer, LC-Halt will return all licences matching the request.
+    Provided with a userId and optionally a bundesland and/or schulnummer, LC-Halt will return all media the user is allowed to access.
     """
     return LicenceResponse(
         userId=userId, bundesland=bundesland, schulnummer=schulnummer, licencedMedia=[]
     )
+
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="LC Halt",
+        version="0.1.0",
+        summary="LC Halt ist a licence holding system.",
+        description="Using this API, media can be assigned to users, schools or federal states. Other services can then access this information and act accordingly.",
+        routes=app.routes,
+    )
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
