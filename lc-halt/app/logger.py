b952e6073593038dbe190db26a8b03a0f7d16798
Paul Reinhardt <paul.reinhardt@xitaso.com>
2025-05-27 13:39:43 +0200

    fix logger at startup

--- lc-halt/app/logger.py
+++ lc-halt/app/logger.py
@@ -3,11 +3,13 @@ import sys
 import os
 
 logger = logging.getLogger("lc-halt")
+logger.setLevel("INFO")
 
 stream_handler = logging.StreamHandler(sys.stdout)
 log_formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(name)s: %(message)s")
 stream_handler.setFormatter(log_formatter)
 logger.addHandler(stream_handler)
+
 
 logger.info("API is starting up")
 
