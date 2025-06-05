5eea157dd69e8062eb6f33048aa595ba9fbc2c3d
Paul Reinhardt <paul.reinhardt@xitaso.com>
2025-06-05 12:20:31 +0200

    feat: add cleanup to tests

--- lc-halt/app/test_media_management_module.py
+++ lc-halt/app/test_media_management_module.py
@@ -1,5 +1,6 @@
 import os
 from fastapi.testclient import TestClient
+import pytest
 
 from app.main import app
 
@@ -7,6 +8,41 @@ client = TestClient(app)
 
 admin_api_key = os.getenv("LC_HALT_ADMIN_API_KEY")
 client_api_key = os.getenv("LC_HALT_CLIENT_API_KEY")
+
+test_data_prefix = "automated-test-"
+
+
+def delete_test_assignments():
+    assignments = client.get(
+        "/admin/media-licence-assignment",
+        headers={"x-api-key": admin_api_key},
+    ).json()
+    test_assignments = [
+        x
+        for x in assignments
+        if (x["user_id"] is not None and x["user_id"].startswith(test_data_prefix))
+        | (
+            x["bundesland_id"] is not None
+            and x["bundesland_id"].startswith(test_data_prefix)
+        )
+        | (x["schul_id"] is not None and x["schul_id"].startswith(test_data_prefix))
+    ]
+    for assignment in test_assignments:
+        client.delete(
+            f"/admin/media-licence-assignment/{assignment['id']}",
+            headers={"x-api-key": admin_api_key},
+        )
+
+
+@pytest.fixture(autouse=True)
+def cleanup():
+    # Code that will run before your test
+
+    # A test function will be run at this point
+    yield
+
+    # Code that will run after your test
+    delete_test_assignments()
 
 
 def test_licenced_media_no_query_params_bad_request():
@@ -30,9 +66,9 @@ def test_assign_media_all_params_bad_request():
         "/admin/media-licence-assignment",
         headers={"x-api-key": admin_api_key},
         json={
-            "user_id": "123",
-            "bundesland_id": "123",
-            "schul_id": "123",
+            "user_id": f"{test_data_prefix}123",
+            "bundesland_id": f"{test_data_prefix}123",
+            "schul_id": f"{test_data_prefix}123",
             "licenced_media": [{"id": "user-medium"}],
         },
     )
@@ -44,11 +80,44 @@ def test_assign_media_only_schul_id_bad_request():
         "/admin/media-licence-assignment",
         headers={"x-api-key": admin_api_key},
         json={
-            "schul_id": "123",
+            "schul_id": f"{test_data_prefix}123",
             "licenced_media": [{"id": "user-medium"}],
         },
     )
     assert response.status_code == 400
+
+
+def test_delete_media_success():
+    # Create test assignment
+
+    assignment_id = client.post(
+        "/admin/media-licence-assignment",
+        headers={"x-api-key": admin_api_key},
+        json={
+            "user_id": f"{test_data_prefix}user",
+            "licenced_media": [{"id": "user-medium"}],
+        },
+    ).json()["id"]
+
+    # Delete created licence assignment
+
+    assert (
+        client.delete(
+            f"/admin/media-licence-assignment/{assignment_id}",
+            headers={"x-api-key": admin_api_key},
+        ).status_code
+        == 204
+    )
+
+    # Check if the id's have actually been deleted
+
+    assert (
+        client.delete(
+            f"/admin/media-licence-assignment/{assignment_id}",
+            headers={"x-api-key": admin_api_key},
+        ).status_code
+        == 404
+    )
 
 
 def test_assign_and_delete_media_smoke_test():
@@ -58,7 +127,7 @@ def test_assign_and_delete_media_smoke_test():
         "/admin/media-licence-assignment",
         headers={"x-api-key": admin_api_key},
         json={
-            "user_id": "automated-test-user",
+            "user_id": f"{test_data_prefix}user",
             "licenced_media": [{"id": "user-medium"}],
         },
     )
@@ -68,7 +137,7 @@ def test_assign_and_delete_media_smoke_test():
         "/admin/media-licence-assignment",
         headers={"x-api-key": admin_api_key},
         json={
-            "bundesland_id": "automated-test-bundesland",
+            "bundesland_id": f"{test_data_prefix}bundesland",
             "licenced_media": [{"id": "bundesland-medium"}],
         },
     )
@@ -78,8 +147,8 @@ def test_assign_and_delete_media_smoke_test():
         "/admin/media-licence-assignment",
         headers={"x-api-key": admin_api_key},
         json={
-            "bundesland_id": "automated-test-bundesland",
-            "schul_id": "automated-test-schule",
+            "bundesland_id": f"{test_data_prefix}bundesland",
+            "schul_id": f"{test_data_prefix}schule",
             "licenced_media": [{"id": "schul-medium"}],
         },
     )
@@ -88,7 +157,7 @@ def test_assign_and_delete_media_smoke_test():
     # Check licenced media responses
 
     licenced_media_user_only_response = client.get(
-        "/licenced-media?user_id=automated-test-user",
+        f"/licenced-media?user_id={test_data_prefix}user",
         headers={"x-api-key": client_api_key},
     )
     user_media = licenced_media_user_only_response.json()["licenced_media"]
@@ -97,7 +166,7 @@ def test_assign_and_delete_media_smoke_test():
     assert len(user_media) == 1
 
     licenced_media_user_and_bundesland_response = client.get(
-        "/licenced-media?user_id=automated-test-user&bundesland_id=automated-test-bundesland",
+        f"/licenced-media?user_id={test_data_prefix}user&bundesland_id={test_data_prefix}bundesland",
         headers={"x-api-key": client_api_key},
     )
     bundesland_media = licenced_media_user_and_bundesland_response.json()[
@@ -109,7 +178,7 @@ def test_assign_and_delete_media_smoke_test():
     assert len(bundesland_media) == 2
 
     licenced_media_user_and_bundesland_and_schule_response = client.get(
-        "/licenced-media?user_id=automated-test-user&bundesland_id=automated-test-bundesland&schul_id=automated-test-schule",
+        f"/licenced-media?user_id={test_data_prefix}user&bundesland_id={test_data_prefix}bundesland&schul_id={test_data_prefix}schule",
         headers={"x-api-key": client_api_key},
     )
     schul_media = licenced_media_user_and_bundesland_and_schule_response.json()[
@@ -121,46 +190,6 @@ def test_assign_and_delete_media_smoke_test():
     assert {"id": "schul-medium"} in schul_media
     assert len(schul_media) == 3
 
-    # Delete created licence assignments
-
-    delete_user_assignment_response = client.delete(
-        f"/admin/media-licence-assignment/{assign_user_medium_response.json()['id']}",
-        headers={"x-api-key": admin_api_key},
-    )
-    assert delete_user_assignment_response.status_code == 204
-
-    delete_bundesland_assignment_response = client.delete(
-        f"/admin/media-licence-assignment/{assign_bundesland_medium_response.json()['id']}",
-        headers={"x-api-key": admin_api_key},
-    )
-    assert delete_bundesland_assignment_response.status_code == 204
-
-    delete_schul_assignment_response = client.delete(
-        f"/admin/media-licence-assignment/{assign_schul_medium_response.json()['id']}",
-        headers={"x-api-key": admin_api_key},
-    )
-    assert delete_schul_assignment_response.status_code == 204
-
-    # Check if the id's have actually been deleted
-
-    delete_user_assignment_again_response = client.delete(
-        f"/admin/media-licence-assignment/{assign_user_medium_response.json()['id']}",
-        headers={"x-api-key": admin_api_key},
-    )
-    assert delete_user_assignment_again_response.status_code == 404
-
-    delete_bundesland_assignment_again_response = client.delete(
-        f"/admin/media-licence-assignment/{assign_bundesland_medium_response.json()['id']}",
-        headers={"x-api-key": admin_api_key},
-    )
-    assert delete_bundesland_assignment_again_response.status_code == 404
-
-    delete_schul_assignment_again_response = client.delete(
-        f"/admin/media-licence-assignment/{assign_schul_medium_response.json()['id']}",
-        headers={"x-api-key": admin_api_key},
-    )
-    assert delete_schul_assignment_again_response.status_code == 404
-
 
 def test_assign_multiple_media_in_one_request_success():
     # Assign media to user, bundesland and school
@@ -169,7 +198,7 @@ def test_assign_multiple_media_in_one_request_success():
         "/admin/media-licence-assignment",
         headers={"x-api-key": admin_api_key},
         json={
-            "user_id": "automated-test-user",
+            "user_id": f"{test_data_prefix}user",
             "licenced_media": [{"id": "medium1"}, {"id": "medium2"}],
         },
     )
@@ -181,7 +210,7 @@ def test_assign_multiple_media_in_one_request_success():
     # Check licenced media responses
 
     licenced_media_only_response = client.get(
-        "/licenced-media?user_id=automated-test-user",
+        f"/licenced-media?user_id={test_data_prefix}user",
         headers={"x-api-key": client_api_key},
     )
     assigned_media = licenced_media_only_response.json()["licenced_media"]
@@ -189,14 +218,6 @@ def test_assign_multiple_media_in_one_request_success():
     assert {"id": "medium1"} in assigned_media
     assert {"id": "medium2"} in assigned_media
     assert len(assigned_media) == 2
-
-    # Delete created licence assignments
-
-    delete_user_assignment_response = client.delete(
-        f"/admin/media-licence-assignment/{assign_medium_response.json()['id']}",
-        headers={"x-api-key": admin_api_key},
-    )
-    assert delete_user_assignment_response.status_code == 204
 
 
 def test_assign_media_consecutively_success():
@@ -206,7 +227,7 @@ def test_assign_media_consecutively_success():
         "/admin/media-licence-assignment",
         headers={"x-api-key": admin_api_key},
         json={
-            "user_id": "automated-test-user",
+            "user_id": f"{test_data_prefix}user",
             "licenced_media": [{"id": "medium1"}],
         },
     )
@@ -218,7 +239,7 @@ def test_assign_media_consecutively_success():
         "/admin/media-licence-assignment",
         headers={"x-api-key": admin_api_key},
         json={
-            "user_id": "automated-test-user",
+            "user_id": f"{test_data_prefix}user",
             "licenced_media": [{"id": "medium2"}],
         },
     )
@@ -229,7 +250,7 @@ def test_assign_media_consecutively_success():
     # Check licenced media responses
 
     licenced_media_only_response = client.get(
-        "/licenced-media?user_id=automated-test-user",
+        f"/licenced-media?user_id={test_data_prefix}user",
         headers={"x-api-key": client_api_key},
     )
     assigned_media = licenced_media_only_response.json()["licenced_media"]
@@ -237,17 +258,3 @@ def test_assign_media_consecutively_success():
     assert {"id": "medium1"} in assigned_media
     assert {"id": "medium2"} in assigned_media
     assert len(assigned_media) == 2
-
-    # Delete created licence assignments
-
-    delete_user_assignment_response = client.delete(
-        f"/admin/media-licence-assignment/{assign_medium1_response.json()['id']}",
-        headers={"x-api-key": admin_api_key},
-    )
-    assert delete_user_assignment_response.status_code == 204
-
-    delete_user_assignment_response = client.delete(
-        f"/admin/media-licence-assignment/{assign_medium2_response.json()['id']}",
-        headers={"x-api-key": admin_api_key},
-    )
-    assert delete_user_assignment_response.status_code == 204
