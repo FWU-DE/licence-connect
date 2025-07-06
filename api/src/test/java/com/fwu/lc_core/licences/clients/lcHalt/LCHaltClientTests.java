0e374293074b1c191420eddc38fd1bc2186fe759
Jan Nash <jan.nash@xitaso.com>
2025-07-06 20:20:01 +0200

    test: add @BeforeEach cleanup to LCHaltClientTests

--- api/src/test/java/com/fwu/lc_core/licences/clients/lcHalt/LCHaltClientTests.java
+++ api/src/test/java/com/fwu/lc_core/licences/clients/lcHalt/LCHaltClientTests.java
@@ -4,7 +4,7 @@ import com.fwu.lc_core.licences.models.ODRLAction;
 import com.fwu.lc_core.shared.LicenceHolder;
 import com.fwu.lc_core.licences.models.ODRLPolicy;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +17,7 @@ import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static com.fwu.lc_core.shared.Constants.API_KEY_HEADER;
@@ -34,13 +35,14 @@ public class LCHaltClientTests {
     @Autowired
     LCHaltClient lchaltClient;
 
-    // FIXME: For this whole test suite to work, the LC-Halt instance must be running.
-    // Ideally, we'd start and stop it in a @BeforeAll and @AfterAll method, by running `bun run:lc-halt:container`
-    // and docker compose down, respectively. I have not yet figured out how to properly do this in a Spring Boot test
-    // context, as we do already have a docker compose for the tests, which is starting the other mock servers.
+    @BeforeEach
+    public void setup() {
+        // Clean up licences from lc halt before each test
+        removeAllTestLicences();
+    }
+
     @Test
     public void RequestPermissions_GivenUserID_Yields_Result() throws ParserConfigurationException, IOException, SAXException {
-
         var expectedUserId = "test_user_id";
         var expectedMediaId = "test_media_id";
         var expectedLicencedMedia = List.of(Map.of("id", expectedMediaId));
@@ -52,27 +54,56 @@ public class LCHaltClientTests {
                 "licenced_media", expectedLicencedMedia
         );
 
-        var webClient = WebClient.builder().baseUrl(lcHaltDomain).build();
-
-        // CAUTION: FIXME: This will add new licences without cleaning up afterwards.
-        //  Thus, the `size()` and the `isIn` checks below will fail if run multiple times.
-        // TODO: Add cleanup to remove test licences from LC-Halt after the test runs.
-        webClient.post()
-                .uri("/admin/media-licence-assignment/")
-                .header(API_KEY_HEADER, lcHaltAdminApiKey)
-                .bodyValue(testLicences)
-                .retrieve()
-                .bodyToMono(String.class)
-                .block();
+        postTestLicences(testLicences);
 
         var permissions = lchaltClient.getPermissions(null, null, null, expectedUserId).block();
 
         assertThat(permissions).isNotNull();
-//        assertThat(permissions.size()).isEqualTo(expectedLicencedMedia.size());
+        assertThat(permissions.size()).isEqualTo(expectedLicencedMedia.size());
         for (ODRLPolicy.Permission p : permissions) {
             assertThat(p.assigner).isEqualTo(LicenceHolder.LC_HALT);
             assertThat(p.action).isEqualTo(ODRLAction.Use);
-//            assertThat(p.target).isIn(expectedLicencedMediaIds);
+            assertThat(p.target).isIn(expectedLicencedMediaIds);
         }
+    }
+
+    private void postTestLicences(Map<String, Object> licences) {
+        var webClient = getLCHaltMediaAssignemtWebClient();
+
+        webClient
+                .post()
+                .bodyValue(licences)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+    }
+
+    private void removeAllTestLicences() {
+        var webClient = getLCHaltMediaAssignemtWebClient();
+
+        var licences = webClient.get()
+                .retrieve()
+                .bodyToFlux(Map.class)
+                .collectList()
+                .block();
+
+        assert(licences != null);
+
+        licences.stream()
+                .map(media -> media.get("id").toString())
+                .forEach(id ->
+                    webClient.delete()
+                            .uri(id)
+                            .retrieve()
+                            .bodyToMono(String.class)
+                            .block()
+                );
+    }
+
+    private WebClient getLCHaltMediaAssignemtWebClient() {
+        return WebClient.builder()
+                .baseUrl(lcHaltDomain + "/admin/media-licence-assignment/")
+                .defaultHeader(API_KEY_HEADER, lcHaltAdminApiKey)
+                .build();
     }
 }
