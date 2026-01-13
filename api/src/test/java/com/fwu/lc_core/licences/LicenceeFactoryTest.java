a1086ce8699b8c6f5ed6c833b3cb2f514de2d46c
Frederik Schlueter <frederik.schlueter@xitaso.com>
2026-01-13 13:47:15 +0000

    FBM-506: ✨ Add schulnummer standortnummer mapping

---
+++ api/src/test/java/com/fwu/lc_core/licences/LicenceeFactoryTest.java
@@ -0,0 +1,53 @@
+package com.fwu.lc_core.licences;
+
+import com.fwu.lc_core.licences.models.Licencee;
+import com.fwu.lc_core.shared.Bundesland;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class LicenceeFactoryTest {
+
+    private final LicenceeFactory licenceeFactory = new LicenceeFactory();
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "BY, 12345, user1, null",
+            "BB, 12-34-567890, user1, DISTRICT_1",
+            "BB, 12-34-999111, user1, null",
+            "null, 12345, user1, null"
+    }, nullValues = "null")
+    void create_valid_licencees(String bundesland, String schulnummer, String userId, String expectedStandort) {
+        // When
+        Licencee licencee = licenceeFactory.create(bundesland, null,  schulnummer, userId);
+
+        // Then
+        assertLicencee(licencee, bundesland, expectedStandort, schulnummer, userId);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "BB, INVALID, user1, Invalid Schulkennung format for BB",
+            "BB, null, user1, Schulkennung must be provided for BB bundesland",
+            "INVALID, 12345, user1, INVALID"
+    }, nullValues = "null")
+    void create_throws_exception_for_invalid_input(String bundesland, String schulnummer, String userId, String expectedMessagePart) {
+        assertThatThrownBy(() -> licenceeFactory.create(bundesland, null, schulnummer, userId))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(expectedMessagePart);
+    }
+
+    private void assertLicencee(Licencee actual, String expectedBundeslandStr, String expectedStandort, String expectedSchulnummer, String expectedUserId) {
+        Bundesland expectedBundesland = null;
+        if (expectedBundeslandStr != null) {
+            expectedBundesland = Bundesland.fromAbbreviation(expectedBundeslandStr);
+        }
+
+        assertThat(actual.bundesland()).isEqualTo(expectedBundesland);
+        assertThat(actual.standortnummer()).isEqualTo(expectedStandort);
+        assertThat(actual.schulnummer()).isEqualTo(expectedSchulnummer);
+        assertThat(actual.userId()).isEqualTo(expectedUserId);
+    }
+}
